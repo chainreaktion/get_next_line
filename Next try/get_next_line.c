9f9b97009ab7e63bd661450f5a12a524f0980d9a
Johanna Schmitz <jschmitz@student.42.fr>
2024-07-20 19:34:08 +0200

    rho lalalaa

--- Next try/get_next_line.c
+++ Next try/get_next_line.c
@@ -6,39 +6,40 @@
 /*   By: jschmitz <jschmitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 19:58:09 by jschmitz          #+#    #+#             */
-/*   Updated: 2024/07/19 22:48:57 by jschmitz         ###   ########.fr       */
+/*   Updated: 2024/07/20 19:01:15 by jschmitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*read_line(int fd, char **line, char *buf_cpy)
+int	read_line(int fd, char **line, char **buf_cpy)
 {
 	int		read_output;
-	char	buffer[BUFFER_SIZE + 1];
+	char 	*buffer;
 
 	read_output = 1;
 	while (read_output != 0)
 	{
+		buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+		if (buffer == NULL)
+			return (-1);
 		read_output = read (fd, buffer, BUFFER_SIZE);
 		if (read_output < 0)
-			return (NULL);
-			//error message missing
-		if (read_output == 0)
+			return (free(buffer), -1);
+		if (ft_strdup(buffer, buf_cpy) == -1)
+			return (free(buffer), -1);
+		if (ft_strchr(buf_cpy, '\n') != 0 || ft_strchr(buf_cpy, '\0') != 0)
 			break ;
-		buf_cpy = ft_strdup(buffer);
-		if (ft_strchr(buf_cpy, '\n') != 0 || ft_strchr(buf_cpy, '\n') != 0)
-			return (buf_cpy);
-		*line = ft_strjoin(*line, buf_cpy);
+		if (ft_strjoin(*line, *buf_cpy, &line) == -1)
+			return (free(buffer), -1);
 	}
-	return (buf_cpy);
+	return (free(buffer), read_output);
 }
 
-char*	last_buffer(char *last_buf)
+int	last_buffer(char *last_buf, char **tmp)
 {
 	int		i;
 	int		j;
-	char	*tmp;
 
 	i = 0;
 	j= 0;
@@ -46,7 +47,7 @@ char*	last_buffer(char *last_buf)
 		i++;
 	tmp = (char *)malloc(sizeof(char) * i + 1);
 	if (tmp == NULL)
-		return (free (last_buf), NULL);
+		return (free (last_buf), -1);
 	while (j <= i)
 	{
 		tmp[j] = last_buf[j];
@@ -54,14 +55,13 @@ char*	last_buffer(char *last_buf)
 	}
 	if (tmp[j - 1] != '\0')
 		tmp[j]= '\0';
-	return (tmp);
+	return (0);
 }
 
-char*	start_nextline(char *buf_cpy)
+int	start_nextline(char *buf_cpy, char **line_start)
 {
 	int	i;
 	int	j;
-	char *line_start;
 
 	i = 0;
 	while (buf_cpy[i] && buf_cpy[i] != '\n')
@@ -69,12 +69,12 @@ char*	start_nextline(char *buf_cpy)
 	j = i + 1;
 	line_start = (char *)malloc(sizeof(char) * (ft_strlen(buf_cpy) + 1));
 	if (line_start == NULL)
-		return (free (buf_cpy), NULL);
+		return (-1);
 	i = 0;
 	while (buf_cpy[j])
-		line_start[i++] = buf_cpy[j++];
-	line_start[i] = '\0';
-	return (line_start);
+		(*line_start)[i++] = buf_cpy[j++];
+	(*line_start)[i] = '\0';
+	return (0);
 }
 
 char	*get_next_line(int fd)
@@ -83,23 +83,20 @@ char	*get_next_line(int fd)
 	char		*buf_cpy;
 	static char	*line_start;
 	char		*last_buf;
+	int			output_signal;
 
 if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 {
-	free (line_start);
-	free (line);
-	free(buf_cpy);
-	return (NULL);
+	return (free_all, NULL);
 }
 	line = "";
 	if (line_start == 0)
 		line_start = "";
-	if (ft_strchr(line_start, '\n') != 0 || ft_strchr(line_start, '\0') != 0)
+	else if (ft_strchr(line_start, '\n') != 0 || ft_strchr(line_start, '\0') != 0)
 	{
-		buf_cpy = strdup(line_start);
-		last_buf = last_buffer(buf_cpy);
-		if (last_buf == NULL)
-			return (NULL);
+		output_signal = border_case(line_start, &line, &buf_cpy);
+		if (output_signal == -1)
+			return (free(line_start), free(buf_cpy), NULL)
 		line = ft_strjoin(line, last_buf);
 		if (ft_strchr(buf_cpy, '\n') != 0)
 			line_start = start_nextline(buf_cpy, line_start);
@@ -107,8 +104,20 @@ if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		free (buf_cpy);
 		return (line);
 	}
-	line = ft_strjoin(line, line_start);
-	buf_cpy = read_line(fd, &line, buf_cpy);
+	output_signal = ft_strjoin(line, line_start, &line);
+	if (output_signal == -1)
+		return (free_all, NULL);
+	output_signal = read_line(fd, &line, &buf_cpy);
+	if (output_signal == -1)
+		return (free_all, NULL);
+	if (output_signal == 0)
+		return (free(buf_cpy), free(line_start), line);
+	if (output_signal < BUFFER_SIZE)
+	{
+		if (ft_strjoin(line, buf_cpy, &line) == -1)
+			return (free(buf_cpy), free(line_start), NULL);
+		return (free(buf_cpy), free(line_start), line);
+	}
 	if (ft_strchr(buf_cpy, '\n') != 0 || ft_strchr(buf_cpy, '\0') != 0)
 	{
 		last_buf = last_buffer(buf_cpy);
@@ -116,7 +125,7 @@ if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 			return (NULL);
 		line = ft_strjoin(line, last_buf);
 		if (ft_strchr(buf_cpy, '\n') != 0)
-			line_start = start_nextline(buf_cpy, line_start);
+			start_nextline(buf_cpy, &line_start);
 	}
 	free (last_buf);
 	free (buf_cpy);
--- Next try/get_next_line_utils.c
+++ Next try/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: jschmitz <jschmitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 17:13:00 by jschmitz          #+#    #+#             */
-/*   Updated: 2024/07/19 21:50:44 by jschmitz         ###   ########.fr       */
+/*   Updated: 2024/07/20 19:06:52 by jschmitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,49 +38,73 @@ char	*ft_strchr(char *s, int c)
 	return (0);
 }
 
-char	*ft_strdup(const char *s)
+int	ft_strdup(const char *s, char **cpy)
 {
-	int		i;
-	char	*cpy;
+	size_t		i;
 
-	cpy = malloc (sizeof(char) * (ft_strlen(s) + 1));
-	if (cpy == NULL)
-		return (NULL);
+	//check that the string s and the pointer to the copy are valid
+	if (s == NULL || cpy == NULL)
+		return (-1);
+	*cpy = malloc (sizeof(char) * (ft_strlen(s) + 1));
+	if (*cpy == NULL)
+		return (-1);
 	i = 0;
 	while (s[i])
 	{
-		cpy[i] = s[i];
+		(*cpy)[i] = s[i];
 		i++;
 	}
-	cpy[i] = '\0';
-	return (cpy);
+	(*cpy)[i] = '\0';
+	return (0);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+int	ft_strjoin(char const *s1, char const *s2, char **res)
 {
 	size_t	i;
 	size_t	len_s1;
 	size_t	len_s2;
-	char	*res;
 
+	if (s1 == NULL || s2 == NULL || res == NULL)
+		return (-1);
 	i = 0;
 	len_s1 = ft_strlen(s1);
 	len_s2 = ft_strlen(s2);
-	res = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
-	if (res == NULL)
-		return (NULL);
+	*res = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	if (*res == NULL)
+		return (-1);
 	while (i < len_s1)
 	{
-		res[i] = s1[i];
+		(*res)[i] = s1[i];
 		i++;
 	}
 	i = 0;
 	while (i < len_s2)
 	{
-		res[i + len_s1] = s2[i];
+		(*res)[i + len_s1] = s2[i];
 		i++;
 	}
-	res[i + len_s1] = '\0';
-	return (res);
+	(*res)[i + len_s1] = '\0';
+	return (free (s1), free (s2), 0);
 }
-test_end(char *)
+int	border_case(char *line_start, char **line, char **buf_cpy)
+{
+	int	return_val;
+
+	if (ft_strchr(line_start, '\0') != 0)
+		return_val = last_buffer(line_start, buf_cpy)
+
+
+	last_buf = last_buffer(buf_cpy);
+
+		return (char *last_buf, char **tmp)
+	else if (ft_strchr(line_start, '\n') != 0)
+	{
+		return_val = strdup(line_start, buf_cpy);
+		if (return_val == -1)
+			return (-1);
+		return_val = start_nextline(buf_cpy, line_start)
+		return (return_val);
+	}
+
+
+}
